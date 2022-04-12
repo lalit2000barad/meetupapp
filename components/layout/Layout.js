e574f3031a674acd87b293caba6aaf38362e719d
Om Ghanshyam Bhamare <62321632+ombhamare4@users.noreply.github.com>
2022-04-12 19:00:56 +0530

    Site Start

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,8 +4,8 @@ import classes from './Layout.module.css';
 function Layout(props) {
   return (
     <div>
-      {/* <MainNavigation /> */}
-      {/* <main className={classes.main}>{props.children}</main> */}
+      <MainNavigation />
+      <main className={classes.main}>{props.children}</main>
     </div>
   );
 }
